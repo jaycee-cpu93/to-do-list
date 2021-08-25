85c845919dc901d19be3991a6447127ac92a1680
jaycee-cpu93 <jamescoding93@gmail.com>
2021-08-25 14:37:58 +0100

    developed my first to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+import TodoForm from './component/TodoForm';
+
+function Todo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "study tonight" },
+    { text: "exercise in the morning" },
+    { text: "listen to my favorite music" }
+  ]);
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
---
+++ src/component/TodoForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import "../style/TodoForm.css";
+
+//to create the to-do input field |_____|
+function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    );
+  }
+
+  export default TodoForm
